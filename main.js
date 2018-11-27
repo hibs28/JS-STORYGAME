80e068a81260df1583d30f91ab1e712eab1231ce
Tech-A06 <hibah28@live.co.uk>
2018-11-27 20:48:54 +0000

    created a menu for the game

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 //Story based game
+const playerName = prompt("Enter your name: ");
+
+while (menuSelect !== ("q" || "g")) {
+  var menuSelect = prompt("Menu \n Type in the options you want to select " + playerName + "\n I - Instructions   G - Game   Q - Quit").toLowerCase(); //had to use var for this 
+
+  switch (menuSelect) {
+    case "i":
+      alert("Instructions");
+      break;
+    case "g":
+      alert("let the games begin")
+      game();
+      break;
+    case "q":
+      alert("Goodbye");
+      window.close();
+      break;
+    default:
+      alert("Please enter the right option");
+      break;
+  }
+}
+
+
+function game() {
+  prompt(playerName + "you're a wizard");
+}
